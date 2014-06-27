919add0e3b8d0033d35c9c67bb3e60d0abe1563f
gongpb <my_program@126.com>
2014-06-27 19:12:51 +0800

    test

---
+++ spring-mvc/src/main/java/com/gongpb/framework/mvc/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.gongpb.framework.mvc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class IndexController {
+	private static Logger logger = LoggerFactory.getLogger(IndexController.class);
+	@RequestMapping("/index.do")
+	public String index() {
+		logger.info("---登录login----");
+		return "login.jsp";
+	}
+}
